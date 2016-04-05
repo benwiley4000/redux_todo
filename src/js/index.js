9ef6a9b865fcaeafb3720c23aac54a52a94b18d1
Ben Wiley <therealbenwiley@gmail.com>
2016-04-05 12:06:00 -0400

    Usage of todo add included in index.js.

--- src/js/index.js
+++ src/js/index.js
@@ -4,10 +4,11 @@ const React = require('react');
 const ReactDOM = require('react-dom');
 const { createStore } = require('redux');
 
-const { counter } = require('./reducer');
+const { counter, todos } = require('./reducer');
 const Counter = require('./components/counter');
 
 const counterStore = createStore(counter);
+const todoStore = createStore(todos);
 
 const render = () => {
   ReactDOM.render(
@@ -30,3 +31,12 @@ const render = () => {
 
 counterStore.subscribe(render);
 render();
+
+console.log(todoStore.getState());
+console.log('Dispatching ADD_TODO');
+todoStore.dispatch({
+  type: 'ADD_TODO',
+  id: 0,
+  text: 'Learn Redux'
+});
+console.log(todoStore.getState());
