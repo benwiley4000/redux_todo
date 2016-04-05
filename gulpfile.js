74a584e10894bd9708c137fa7d2913781364cab3
Ben Wiley <therealbenwiley@gmail.com>
2016-04-05 00:10:04 -0400

    Redux project initialized with tests.

---
+++ gulpfile.js
@@ -0,0 +1,29 @@
+var gulp = require('gulp');
+var browserify = require('browserify');
+var source = require('vinyl-source-stream');
+
+gulp.task('copy', ['copy:html']);
+
+gulp.task('copy:html', function () {
+  return gulp.src('src/*.html')
+    .pipe(gulp.dest('public/'));
+});
+
+gulp.task('browserify', function() {
+  return browserify('src/js/index.js')
+    .transform('babelify', { presets: ['es2015', 'react'] })
+    .bundle()
+    .on('error', function (err) {
+      console.error(err);
+      this.emit('end');
+    })
+    .pipe(source('main.js'))
+    .pipe(gulp.dest('public/'));
+});
+
+gulp.task('watch', function () {
+  gulp.watch('src/index.html', ['copy:html']);
+  gulp.watch('src/js/**/*.js', ['browserify']);
+});
+
+gulp.task('default', ['copy', 'browserify', 'watch']);
