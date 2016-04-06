29874146546aaf87e22099ab424b3e406dde5346
Ben Wiley <therealbenwiley@gmail.com>
2016-04-05 20:04:50 -0400

    Now using Provider from ReactRedux.

--- src/js/index.js
+++ src/js/index.js
@@ -2,26 +2,12 @@
 
 const React = require('react');
 const ReactDOM = require('react-dom');
+const { Provider } = require('react-redux');
 const { createStore } = require('redux');
 
 const reducer = require('./reducers/todoapp');
 
 const TodoApp = require('./components/TodoApp');
-
-class Provider extends React.Component {
-  getChildContext () {
-    return {
-      store: this.props.store
-    };
-  }
-
-  render () {
-    return this.props.children;
-  }
-}
-Provider.childContextTypes = {
-  store: React.PropTypes.object
-};
 
 ReactDOM.render(
   <Provider store={createStore(reducer)}>
